bd8aeaeac4b6c1324b35fd976b3f5ddfcbf371cd
RAJESH REDDY <rajeshreddy@RAJESHs-MacBook-Air.local>
2026-03-15 19:01:07 +0530

    updated backend URL for deployment

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api"
+  baseURL: "https://urbanride-qg4a.onrender.com/api"
 });
 
 // attach JWT token automatically
